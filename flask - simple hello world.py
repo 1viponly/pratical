63512bc1ff08e014a6bc07df7cb29ac5bdf62a9d
1viponly <sonivipin039@gmail.com>
2020-04-09 23:18:27 +0530

    simple flask code

---
+++ flask - simple hello world.py
@@ -0,0 +1,20 @@
+# importing flask
+from flask import Flask
+
+# init app & constructor
+app = Flask(__name__)
+
+# rendering (URL)
+@app.route('/')
+def hello():
+    return "Hello, Flask!"
+
+# rendering (URL)
+@app.route('/hello/<name>')
+def hello(name):
+    return "Hello, %s!" % name
+
+
+# main function
+if __name__ == '__main__':
+    app.run(debug = True)

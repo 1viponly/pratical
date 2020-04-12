b5930fcb83ddb78083ec20a10b2f5ca5ae26c0e2
1viponly <63418493+1viponly@users.noreply.github.com>
2020-04-13 00:47:39 +0530

    more app

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-None
+# importing flask
+from flask import Flask
+
+# init app & constructor
+app = Flask(__name__)
+
+# rendering (URL)
+@app.route('/')
+def hello():
+    return "Hello, Flask!"
+
+# rendering (URL)
+@app.route('/hello/<name>')
+def check(name):
+    return "Hello, %s!" % name
+
+
+# main function
+if __name__ == '__main__':
+    app.run(debug = True)
